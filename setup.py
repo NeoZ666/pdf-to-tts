4bf755c03ccdeb884847ea2bde62ecfc20e1abd6
NeoZ666 <neoz.blockchain@gmail.com>
2023-04-28 02:17:22 +0530

    Initial Commit
    Version 1

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mycli',
+    version='0.1.0',
+    author='NeoZ',
+    author_email='neoz.blockchain@gmail.com',
+    description='A CLI tool for converting Book PDFs into mp3 Audio files',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'gTTS',
+        'PyPDF2',
+        'pathlib'
+    ],
+    entry_points={
+        'console_scripts': [
+            'mycli=mycli.cli:main',
+        ],
+    },
+    include_package_data=True,
+    package_data={'': ['LICENSE', 'README.rst']},
+    data_files=[
+        ()
+    ]
+)
